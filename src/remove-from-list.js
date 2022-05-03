4facc8bbcf5e3467bc2c48aef02a6fe5eaa5701c
unknown <avtoroku66@gmail.com>
2022-05-03 03:03:33 +0300

    feat: solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if(l === null) return l;
+  while(l.value === k) l = l.next;
+  thisNode = l;
+  nextNode = thisNode.next;
+  while(nextNode != null) {
+    if(nextNode.value === k) {
+      thisNode.next = nextNode.next;
+      if(thisNode.next === null) break;
+    }
+    thisNode = thisNode.next;
+    nextNode = thisNode.next;       
+  }
+  return l;
 }
 
 module.exports = {
